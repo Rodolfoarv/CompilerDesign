81f968c2eafe15cd1acd6248a847f3222a31e242
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-08-24 21:40:44 -0500

    First implementation of Generators

---
+++ Generator/GeneratorExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Pow2Generator {
+	public IEnumerable<int> Start() {
+		var c = 1;
+		while (c < 1000000) {
+			yield return c;
+			c *= 2; 
+		}
+	}
+}
+
+public class GeneratorExample{
+
+	public static void Main(String[] args) {
+
+		IEnumerable<int> i = new Pow2Generator ().Start ();
+		var e = i.GetEnumerator ();
+		while (e.MoveNext ()) {
+			Console.WriteLine (e.Current);
+		}
+
+	}
+
+}
+
+
