00552f6fdea1e7120f0d8c0dd2d6fe26414eee97
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-08-30 23:59:52 -0500

    Implementation with a foreach

--- Generator/GeneratorExample.cs
+++ Generator/GeneratorExample.cs
@@ -1,28 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Pow2Generator {
-	public IEnumerable<int> Start() {
+public class Pow2Generator
+{
+	public IEnumerable<int> Start ()
+	{
 		var c = 1;
 		while (c < 1000000) {
 			yield return c;
-			c *= 2; 
+			c *= 2;
 		}
 	}
 }
 
-public class GeneratorExample{
-
-	public static void Main(String[] args) {
-
-		IEnumerable<int> i = new Pow2Generator ().Start ();
-		var e = i.GetEnumerator ();
-		while (e.MoveNext ()) {
-			Console.WriteLine (e.Current);
+public class GeneratorExample
+{
+	public static void Main (String[] args)
+	{
+		foreach (var i in new Pow2Generator().Start()) {
+			Console.WriteLine (i);
 		}
-
 	}
-
 }
-
-
