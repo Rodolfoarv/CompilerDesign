1ed81b4870f21632d495a7de56cca61ddb469d68
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-09-05 02:27:41 -0500

    For with terminating bound and condition

--- Fortran77/circle.f
+++ Fortran77/circle.f
@@ -20,12 +20,18 @@
 
 !     Do loop
 
-
       do 10 i = 1, n
         s = s + 1
         write (*, *) 'i =', i
         write (*, *) 's =', s
 10    continue
+
+!     the first argument is the initial value of i, the second
+!     is the terminating bound
+!     the third is the increment step
+      do 20 i = 10, 1, -2
+        write (*, *) 'i=', i
+20    continue
 
 
       stop
