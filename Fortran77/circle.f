b97778e667fc6021aebb55605573977c9282019d
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-09-05 01:59:15 -0500

    Introduced parameter statement

--- Fortran77/circle.f
+++ Fortran77/circle.f
@@ -1,12 +1,13 @@
       program circle
       real r, area
+      parameter (pi = 3.14159)
 
 !     This program reads a real number r and prints
 !     the area of the circle with radius r.
 
       write (*, *) 'Give radius r:'
       read  (*, *) r
-      area = 3.14159 * r * r
+      area = pi * r * r
       write (*, *) 'Area =', area
 
       stop
