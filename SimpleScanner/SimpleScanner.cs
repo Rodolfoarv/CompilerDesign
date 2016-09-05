cfcd12c19a9d0b8517fd9850f3a1f2e31c8c1ca4
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-09-05 01:43:25 -0500

    Updated Simple Scanner

--- SimpleScanner/SimpleScanner.cs
+++ SimpleScanner/SimpleScanner.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
 public class SimpleScanner {
 	public static void Main() {
-		var regex = new Regex (@"/[*](.|\n)*?[*]/");
+		var regex = new Regex (@"(/[*]./?[*]/)|(.)", RegexOptions.Singleline);
 		var input = File.ReadAllText ("/home/rodolfo/Documents/CompilerDesign/SimpleScanner/Test.c");
 		foreach (Match m in regex.Matches(input)) {
-			Console.WriteLine ("{0} {1} {2}", m.Value, m.Index, m.Length);
+			if (m.Groups [2].Length != 0) { //m.groups[1] refers to the first part (/[*]./?[*]/)|(.+)
+				Console.Write (m.Groups [2].Value);
+			}
+
+			//Console.WriteLine ("{0} {1} {2}", m.Value, m.Index, m.Length);
 		}
 	}
 }
+
+//Group capturing
+
+
+// A group that we will be able to define as an index
