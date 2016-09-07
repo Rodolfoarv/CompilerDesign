6f225c9c4d093350ec78e3facf4b4fd437b588af
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-09-07 09:20:29 -0500

    Added replace exercise

--- RegexPractice/RegexPractice/Solution.cs
+++ RegexPractice/RegexPractice/Solution.cs
@@ -10,27 +10,32 @@ public class Solution
 	}
 
 	public static void Main(){
+//		string pattern = @"([a-zA-z]+) (\d+)";
+//		MatchCollection matches = Regex.Matches ("June 24, August 9, Dec 12", pattern);
+//
+//		Console.WriteLine ("{0} matches", matches.Count);
+//
+//		foreach (Match match in matches) {
+//			Console.WriteLine ("Match {0} at index [{1}, {2}]", 
+//				match.Value,
+//				match.Index,
+//				match.Index + match.Length);
+//		}
+//
+//		foreach (Match match in matches) {
+//			GroupCollection data = match.Groups;
+//			Console.WriteLine("{0} groups captured in {1}", data.Count, match.Value);
+//			Console.WriteLine("Month: " + data[1] + ", Day: " + data[2]);
+//			Console.WriteLine("Month found at[{0}, {1})", 
+//				data[1].Index, 
+//				data[1].Index + data[1].Length);
+//
+//		}
+
 		string pattern = @"([a-zA-z]+) (\d+)";
-		MatchCollection matches = Regex.Matches ("June 24, August 9, Dec 12", pattern);
-
-		Console.WriteLine ("{0} matches", matches.Count);
-
-		foreach (Match match in matches) {
-			Console.WriteLine ("Match {0} at index [{1}, {2}]", 
-				match.Value,
-				match.Index,
-				match.Index + match.Length);
-		}
-
-		foreach (Match match in matches) {
-			GroupCollection data = match.Groups;
-			Console.WriteLine("{0} groups captured in {1}", data.Count, match.Value);
-			Console.WriteLine("Month: " + data[1] + ", Day: " + data[2]);
-			Console.WriteLine("Month found at[{0}, {1})", 
-				data[1].Index, 
-				data[1].Index + data[1].Length);
-
-		}
+		string replacedString = Regex.Replace ("June 24, August 9, Dec 12",
+			                        pattern, @"$2 of $1");
+		Console.WriteLine (replacedString);
 	}
 }
 
