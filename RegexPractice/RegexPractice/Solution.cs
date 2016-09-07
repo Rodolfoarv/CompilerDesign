5fe51e42c1b7358028ad3f59bee0d556abf52561
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-09-07 08:48:25 -0500

    Added date exercise with simple match RegexPractice

---
+++ RegexPractice/RegexPractice/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class Solution
+{
+	public Solution ()
+	{
+	
+	}
+
+	public static void Main(){
+		string pattern = @"([a-zA-Z]+) (\d+)";
+		Match result = Regex.Match("June 24", pattern);
+
+
+		if (result.Success) {
+			Console.WriteLine("Match at index [{0}, {1})", 
+				result.Index,
+				result.Index + result.Length);
+
+			Console.WriteLine("Match: {0}", result.Value);
+
+			while (result.Success) {
+				Console.WriteLine("Match: {0}", result.Value);
+				result = result.NextMatch();
+			}
+		}
+	}
+}
+
+
